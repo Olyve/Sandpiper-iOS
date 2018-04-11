65d72f34dbbbb16370479e8560bc1cc03a558b0f
Sam Galizia <samgalizia@gmail.com>
2018-04-11 11:38:36 -0700

    WIP

--- Sandpiper/LoginViewController.swift
+++ Sandpiper/LoginViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+  @IBOutlet weak var emailInput: UITextField!
+  @IBOutlet weak var passwordInput: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  @IBAction func endEditing(_ sender: UITapGestureRecognizer) {
+    emailInput.resignFirstResponder()
+    passwordInput.resignFirstResponder()
   }
-
-
+  
 }
 
